39d35562e1ce38a4ac2dd925dc310b5310906b99
Arriero82 <gabriel.vullioud@gmail.com>
2022-04-28 13:47:10 +0000

    finish game with console operation

--- script.js
+++ script.js
@@ -2,46 +2,78 @@ function computerPlay(){
     let randomChoice = Math.floor((Math.random()*3));
     const choices = ['rock', 'paper', 'scissors'];
     let computerChoice = choices[randomChoice];
+    return computerChoice;
 }
+let computerSelection = computerPlay();
+let playerCount = 0;
+let computerCount = 0;
 
 function playRound(playerSelection, computerSelection) {
+
     if(playerSelection=='rock'){
         if(computerSelection=='rock'){
             console.log('it\'s a tie');
         };
         if(computerSelection=='paper'){
             console.log('You lose, paper wraps the rock');
+            computerCount+=1;
         };
         if(computerSelection=='scissors'){
             console.log('You win, rock smashes scissors');
+            playerCount+=1;
         };
 
     }
     if(playerSelection=='paper'){
         if(computerSelection=='rock'){
             console.log('You win, paper wraps the rock');
+            playerCount+=1;
         };
         if(computerSelection=='paper'){
             console.log('it\'s a tie');
         };
         if(computerSelection=='scissors'){
             console.log('You lose, paper is not match against scissors');
+            computerCount+=1;
         };
     }
     if(playerSelection=='scissors'){
         if(computerSelection=='rock'){
             console.log('You lose, rock smashes scissors');
+            computerCount+=1;
         };
         if(computerSelection=='paper'){
             console.log('You win, paper is not match against scissors');
+            playerCount+=1;
         };
         if(computerSelection=='scissors'){
             console.log('it\'s a tie');
         };
     }
 }
+/*
+function game (){
+    for(i=0;i<5;i++){
+        computerSelection = computerPlay();
+        let playerSelection = (prompt('Write your choice rock, paper or scissors')).toLowerCase();
+        playRound(playerSelection, computerSelection)
+    }
+    console.log(playerCount);
+    console.log(computerCount);
+}
+*/
 
-  
-  const playerSelection = (prompt('Write your choice rock, paper or scissors')).toLowerCase();
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+function game(){
+    while(playerCount<5 && computerCount<5){
+        computerSelection = computerPlay();
+        let playerSelection = (prompt('Write your choice rock, paper or scissors')).toLowerCase();
+        playRound(playerSelection, computerSelection);
+        console.log("Player wins "+playerCount);
+        console.log("Computer wins "+computerCount);
+    }
+
+}
+
+game();
+
+
